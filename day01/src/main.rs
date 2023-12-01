a04c1eed114de9f62fd07b6fc7f0b6401e973833
David Beauchamp <davidabeauchamp@gmail.com>
2023-11-30 23:27:04 -0800

    day01 part2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -13,17 +13,54 @@ fn main() {
 }
 
 fn parse_values(line: &str) -> u32 {
-    let mut digits: String = line
-        .chars()
-        .filter(|c| c.is_digit(10))
-        .into_iter()
-        .collect();
+    let mut nums = Vec::new();
+    let mut word = "".to_string();
+    for c in line.chars() {
+        if c.is_digit(10) {
+            word = "".to_string();
+            nums.push(c.to_digit(10).unwrap());
+            continue;
+        }
 
-    let num = if digits.len() == 1 {
-        digits.clone() + &digits
-    } else {
-        [digits.remove(0), digits.remove(digits.len() - 1)].iter().collect()
-    };
+        word.push(c);
+        if let Some(num) = word_to_number(&word) {
+            // incase the last letter of a word is the beginning of next word
+            word = word.pop().unwrap().to_string();
+            nums.push(num);
+        }
+    }
 
-    num.parse::<u32>().unwrap()
+    nums[0]*10 + nums.last().unwrap()
+}
+
+fn word_to_number(word: &str) -> Option<u32> {
+    if word.contains("one") {
+        return Some(1);
+    }
+    if word.contains("two") {
+        return Some(2);
+    }
+    if word.contains("three") {
+        return Some(3);
+    }
+    if word.contains("four") {
+        return Some(4);
+    }
+    if word.contains("five") {
+        return Some(5);
+    }
+    if word.contains("six") {
+        return Some(6);
+    }
+    if word.contains("seven") {
+        return Some(7);
+    }
+    if word.contains("eight") {
+        return Some(8);
+    }
+    if word.contains("nine") {
+        return Some(9);
+    }
+
+    None
 }
